d9395a52c316adf4c3b160ea96e3222f20555aba
unknown <cthelot@ensc.fr>
2020-04-26 16:39:05 +0200

    nouveau dossier

---
+++ Projet/_projet/Professeur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _projet
+{
+    class Professeur
+    {
+        public Matiere[] matieres;
+
+        public Professeur(string Nom, string Prenom, Projet[] Proj) : base(Nom, Prenom, Proj) { }
+
+        public Professeur(string Nom, string Prenom, Projet[] Proj, Matiere[] Matiere) : base(Nom, Prenom, Proj)
+        {
+            matieres = Matiere;
+        }
+
+        public override string ToString()
+        {
+            string res = " ";
+            res = res + "Nom : " + nom + "/n";
+            res = res + "Prénom : " + prenom + "/n";
+            res = res + "Liste des matières enseignées : "
+                for (int j = 0; j < matieres.Length; j++)
+            {
+                res = res + matieres[j] + " ; ";
+            }
+            res = res + "/nListe des projets en cours : /n";
+            for (int i = 1; i < projet.Length + 1; i++)
+            {
+                res = res + "Projet n°" + i + " : " + projet[i - 1] + "/n";
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Projet/_projet/Projet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _projet
+{
+    class Projet
+    {
+        private double _duree;
+        private bool _sujetLibre;
+        private Eleve[] _eleves;
+
+    }
+}
